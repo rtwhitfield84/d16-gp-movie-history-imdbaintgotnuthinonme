71543e110866fd428df7808010c7dff9d6071e01
Ali Kimbrell <alexandrakimbrell@gmail.com>
2016-11-21 11:37:58 -0600

    Javascripts.  User login.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// $("#auth-btn").click(function() {
+//   console.log("clicked auth");
+//   user.logInGoogle()
+//   .then(function(result){
+//     let user = result.user;
+//     console.log('logged in user', user.uid);
+//     $("#auth-btn").addClass('is-hidden');
+//     $("#logout").removeClass('is-hidden');
+//     loadSongsToDOM();
+//   });
+// });
---
+++ js/user.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let firebase = require("./firebaseConfig");
+let provider = new firebase.auth.GoogleAuthProvider();
+let currentUser = null;
+
+// firebase.auth().onAuthStateChanged(function(user){
+// 	if(user){
+// 		currentUser = user.uid;
+// 		console.log('currentUser logged in?', currentUser);
+// 	} else {
+// 		currentUser = null;
+// 		console.log('currentUser logged out?', currentUser);
+// 	}
+// });
+
+// function logInGoogle() {
+// 	return firebase.auth().signInWithPopup(provider);
+// }
+
+// function logOut(){
+// 	return firebase.auth().signOut();
+// }
+
+// function getUser(){
+// 	return currentUser;
+// }
+
+// module.exports = { logInGoogle, logOut, getUser };
