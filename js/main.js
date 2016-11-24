6eabf0d1f299a6f21bb1d1185c0134b9f3e68200
Mark Ellis <markellisdev@gmail.com>
2016-11-23 19:15:53 -0600

    untracked functionality improved

--- js/main.js
+++ js/main.js
@@ -4,21 +4,34 @@ let signIn = require("./user");
 let Handlebars = require('hbsfy/runtime');
 let untrackedResults = require("./Untracked.js");
 let populateCards = require('./dom-builder.js');
+let dbInteractions = require('./db-interactions.js');
 
-// getMovies().then (function(data){
-// 	(createCards(data));
-// });
+dbInteractions.getMovies().then (function(data){
+	(populateCards.createCards(data));
+});
 
-$("#searchView").hide();
-$("#untrackedView").hide();
+$("#unwatchedView").hide();
+$("#watchedView").hide();
 $("#favoritesView").hide();
 $("#signin").click(signIn.logInGoogle);
 $("#logout").click(signIn.logOut);
 
+/*-- Search Button click--*/
 $("#search").click(function(){
-	console.log("button clicked" );
 	let input = $("#searchBar").val();
+	$("#mainView").show();
 	untrackedResults.getUntracked(input).then(function(data){
 		populateCards.createCards(data);
 	});
+});
+
+/*-- Show Untracked click --*/
+$("#untracked").click(function() {
+	console.log("Untracked button clicked");
+	$("#unwatchedView").hide();
+	$("#watchedView").hide();
+	$("#favoritesView").hide();
+	$("#unwatchedSpan").hide();
+	$("#watchedSpan").hide();
+	$("#favoritesSpan").hide();
 });
